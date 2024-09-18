33afe85d448abb675b2236e092f9c6e74f202037
sinceforYy <1017657683@qq.com>
2024-09-18 17:12:48 +0800

    fix(csr): fix m/henvcfg wmask when enable Zicbo extension and fix csr read/write
    * m/henvcfg.CBIE = 2 is reserved

--- src/isa/riscv64/system/priv.c
+++ src/isa/riscv64/system/priv.c
@@ -258,13 +258,34 @@ static inline word_t* csr_decode(uint32_t addr) {
 #define HSTATUS_WMASK 0
 #endif
 
-#define MENVCFG_WMASK_STCE MUXDEF(CONFIG_RV_SSTC, (0x1UL << 63), 0)
-#define MENVCFG_WMASK_PBMTE MUXDEF(CONFIG_RV_SVPBMT, (0x1UL << 62), 0)
-#define MENVCFG_WMASK_DTE  MUXDEF(CONFIG_RV_SSDBLTRP, (0x1UL << 59), 0)
+#define MENVCFG_RMASK_STCE  (0x1UL << 63)
+#define MENVCFG_RMASK_PBMTE (0x1UL << 62)
+#define MENVCFG_RMASK_DTE   (0x1UL << 59)
+#define MENVCFG_RMASK_CBZE  (0x1UL <<  7)
+#define MENVCFG_RMASK_CBCFE (0x1UL <<  6)
+#define MENVCFG_RMASK_CBIE  (0x3UL <<  4)
+#define MENVCFG_RMASK (    \
+  MENVCFG_RMASK_STCE     | \
+  MENVCFG_RMASK_PBMTE    | \
+  MENVCFG_RMASK_DTE      | \
+  MENVCFG_RMASK_CBZE     | \
+  MENVCFG_RMASK_CBCFE    | \
+  MENVCFG_RMASK_CBIE       \
+)
+
+#define MENVCFG_WMASK_STCE  MUXDEF(CONFIG_RV_SSTC,     (0x1UL << 63), 0)
+#define MENVCFG_WMASK_PBMTE MUXDEF(CONFIG_RV_SVPBMT,   (0x1UL << 62), 0)
+#define MENVCFG_WMASK_DTE   MUXDEF(CONFIG_RV_SSDBLTRP, (0x1UL << 59), 0)
+#define MENVCFG_WMASK_CBZE  MUXDEF(CONFIG_RV_CBO,      (0x1UL <<  7), 0)
+#define MENVCFG_WMASK_CBCFE MUXDEF(CONFIG_RV_CBO,      (0x1UL <<  6), 0)
+#define MENVCFG_WMASK_CBIE  MUXDEF(CONFIG_RV_CBO,      (0x3UL <<  4), 0)
 #define MENVCFG_WMASK (    \
   MENVCFG_WMASK_STCE     | \
   MENVCFG_WMASK_PBMTE    | \
-  MENVCFG_WMASK_DTE        \
+  MENVCFG_WMASK_DTE      | \
+  MENVCFG_WMASK_CBZE     | \
+  MENVCFG_WMASK_CBCFE    | \
+  MENVCFG_WMASK_CBIE       \
 )
 #define HENVCFG_WMASK MENVCFG_WMASK
 
@@ -913,11 +934,7 @@ if (is_read(hgeie))          { return hgeie->val & ~(0x1UL);}
 if (is_read(hip))            { return ((get_mip() & HIP_RMASK) | (hvip->val & MIP_VSSIP)) & (mideleg->val | MIDELEG_FORCED_MASK); }
 if (is_read(hie))            { return mie->val & HIE_RMASK & (mideleg->val | MIDELEG_FORCED_MASK);}
 if (is_read(hvip))           { return hvip->val & HVIP_MASK;}
-if (is_read(henvcfg))     {
-  uint64_t henvcfg_out = henvcfg->val;
-  henvcfg_out &= menvcfg->val & MENVCFG_WMASK;
-  return henvcfg_out & HENVCFG_WMASK;
-}
+if (is_read(henvcfg))        { return henvcfg->val & (menvcfg->val | ~MENVCFG_RMASK) & HENVCFG_WMASK; }
 #ifdef CONFIG_RV_AIA
 if (is_read(hvien))          { return hvien->val & HVIEN_MSAK; }
 #endif
@@ -1140,13 +1157,15 @@ static inline void csr_write(word_t *dest, word_t src) {
   else if(is_write(hip)) { hvip->val = mask_bitset(hvip->val, HIP_WMASK & (mideleg->val | MIDELEG_FORCED_MASK), src); }
   else if(is_write(hvip)) { hvip->val = mask_bitset(hvip->val, HVIP_MASK, src); }
   else if(is_write(henvcfg)){
-    henvcfg->val = mask_bitset(henvcfg->val, HENVCFG_WMASK, src);
+    henvcfg->val = mask_bitset(henvcfg->val, HENVCFG_WMASK & (~MENVCFG_WMASK_CBIE), src);
+    if ((src & MENVCFG_WMASK_CBIE) != (0x20 & MENVCFG_WMASK_CBIE)) {
+      henvcfg->val = mask_bitset(henvcfg->val, MENVCFG_WMASK_CBIE, src);
+    }
   #ifdef CONFIG_RV_SSDBLTRP
     if(henvcfg->dte == 0) {
       vsstatus->sdt = 0;
     }
   #endif // CONFIG_RV_SSDBLTRP
-
   }
 #ifdef CONFIG_RV_AIA
   else if (is_write(hvien)) { hvien->val = mask_bitset(hvien->val, HVIEN_MSAK, src); }
@@ -1223,7 +1242,10 @@ static inline void csr_write(word_t *dest, word_t src) {
     if (src & MSTATUS_WMASK_SDT) { mstatus->sie = 0; }
   #endif //CONFIG_RV_SSDBLTRP
   }else if(is_write(menvcfg)) {
-    menvcfg->val = mask_bitset(menvcfg->val, MENVCFG_WMASK, src);
+    menvcfg->val = mask_bitset(menvcfg->val, MENVCFG_WMASK & (~MENVCFG_WMASK_CBIE), src);
+    if ((src & MENVCFG_WMASK_CBIE) != (0x20 & MENVCFG_WMASK_CBIE)) {
+      menvcfg->val = mask_bitset(menvcfg->val, MENVCFG_WMASK_CBIE, src);
+    }
 #ifdef CONFIG_RV_SSDBLTRP
     if(menvcfg->dte == 0) {
       mstatus->sdt = 0;
