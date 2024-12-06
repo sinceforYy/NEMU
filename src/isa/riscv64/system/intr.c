30e329d93926e66d5e2896a56e1e399815e96c32
sinceforYy <1017657683@qq.com>
2024-12-06 12:08:25 +0800

    fix(intr): fix vs intr no

--- src/isa/riscv64/system/intr.c
+++ src/isa/riscv64/system/intr.c
@@ -145,11 +145,17 @@ word_t raise_intr(word_t NO, vaddr_t epc) {
     delegM = !delegS && !delegVS && !isNMI;
   }
 #endif
+  printf("NO=%lx, delegS=%x, delegVS=%x, delegM=%x\n", NO, delegS, delegVS, delegM);
+#ifdef CONFIG_RV_IMSIC
+  printf("mtopi=%d, stopi=%d, vstopi=%d\n", mtopi->iid, stopi->iid, vstopi->iid);
+#endif
   bool vs_EX_DT = MUXDEF(CONFIG_RV_SSDBLTRP, delegVS && vsstatus->sdt, false);
   m_EX_DT = MUXDEF(CONFIG_RV_SMDBLTRP, delegM && mstatus->mdt, false);
   if ((delegVS && !vs_EX_DT) || (virtualInterruptIsHvictlInject && !isNMI)){
 #ifdef CONFIG_RV_IMSIC
-    if ((NO & INTR_BIT) && (NO & (MIP_SSIP | MIP_STIP | MIP_SEIP))) {
+    int vs_guest_no = NO & 0xff;
+    bool vs_host_no = (vs_guest_no == 2) || (vs_guest_no == 6) || (vs_guest_no == 10);
+    if ((NO & INTR_BIT) && vs_host_no) {
       vscause->val = ((NO & (~INTR_BIT)) - 1) | INTR_BIT;
     } else {
       vscause->val = NO;
